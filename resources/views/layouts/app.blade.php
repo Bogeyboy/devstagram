3da48b5a27ad03c29d8ded6eb91619bba7e3fe5b
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-05-05 20:21:07 +0200

    Clase 49

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Página principal</title>
+    </head>
+    <body>
+        <nav>
+            <a href="/">Principal</a>
+            <a href="/nosotros">Nosotros</a>
+            <a href="/tienda-virtual">Tienda Virtual</a>
+        </nav>
+        <h1>@yield('titulo')</h1>
+        <hr>
+        @yield('contenido')
+    </body>
+</html>
--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Página principal</title>
+@section('titulo')
+    Página principal
+@endsection
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-    </head>
-    <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda-virtual">Tienda Virtual</a>
-        </nav>
-        <h1>Esta es la página principal</h1>
-
-    </body>
-</html>
+@section('contenido')
+    Contenido de la página principal
+@endsection
