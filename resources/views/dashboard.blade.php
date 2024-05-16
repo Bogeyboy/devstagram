1befe02984d8e03b797a65389e3eeb90e9f1eed6
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-05-16 15:35:32 +0200

    Clase 89

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Página sobre tu cuenta
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
+            <div class="w-8/12 lg:w-6/12 px-5">
                 <p>
                     <img src="{{ asset('img/usuario.svg') }}" alt="Imagen del usuario"/>
                 </p>
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 md:flex md:flex-col md:justify-center">
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:py-10">
                 {{-- {{dd($user)}} --}}
                 <p class="text-gray-700 text-2xl">{{-- Nombre de usuario --}}
                     {{ $user->username }}
                 </p>
-                <p class="text-gray-800 text-sm mb-3 font-bold">{{-- Seguidores --}}
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">{{-- Seguidores --}}
                     0<span class="font-normal"> Seguidores</span>
                 </p>
                 <p class="text-gray-800 text-sm mb-3 font-bold">{{-- Seguidos --}}
