4a1d6b64d3bcb1f511a1a977e8f24607c65c42c6
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-05-22 20:55:49 +0200

    Clase 115

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,14 +33,18 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
 
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl::grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="">
-                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
+        @if ($posts->count()>0)
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl::grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a href="">
+                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <div class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones todavía</div>
+        @endif
     </section>
 @endsection
