a0d257cb19f04ad066b7278c07a8e5287b19c49f
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-05-30 14:44:19 +0200

    Clase 144

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Validation\Rule;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class PerfilController extends Controller 
 {
@@ -24,15 +27,7 @@ class PerfilController extends Controller
         //MODIFICAMOS EL REQUEST PARA QUE FUNCIONE LA VALIDACIÓN DE LARAVEL
         $request->request->add(['username' => Str::slug($request->username)]); //CON SLUG CONVIERTE LOS ESPACIOS EN URL
 
-        /* $request->validate([
-            'username' => ['required',
-                            'unique:users,username'.auth()->user()->id,// Excluye de la comprobación el username de la persona logada
-                            'min:3',
-                            'max:20'],
-                            'not_in:twitter,editar-perfil'
-        ]); */
-
-        $request->validate([
+        $request->validate([//Validamos el campo username
             'username' => [
                 'required',
                 //'unique:users,username' . auth()->user()->id, // Excluye de la comprobación el username de la persona logada
@@ -42,5 +37,25 @@ class PerfilController extends Controller
                 'not_in:twitter,editar-perfil'
             ],
         ]);
+
+        if($request->imagen){
+            $manager = new ImageManager(new Driver());
+
+            $imagen = $request->file('imagen');
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+            $imagenServidor = $manager->read($imagen);
+            $imagenServidor->scale(1000, 1000);
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+        //Guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        //Redireccionamos al usuario
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
