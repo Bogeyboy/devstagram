246541ab2378a724af4285e7ac627821cf35a552
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-04 22:53:08 +0200

    Clase 151

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 
 class FollowerController extends Controller
 {
     //    public function store(User $user, Request $request)//$user es el perfil que se está visitando
     public function store(User $user)
     {
-        $user->followers()->attach( auth()->user()->id );/* Usamos attach para hacer las relaciones con la misma tabla */
+        $user->followers()->attach( auth()->user()->id, ['created_at' => Carbon::now(), 'updated_at' => Carbon::now()] );/* Usamos attach para hacer las relaciones con la misma tabla */
 
         return back();//Volvemos a la página anterior
     }
