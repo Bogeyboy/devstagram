7016b3bf62146d5561955fcff8064ed2f9054ac2
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-06 13:42:28 +0200

    Clase 158

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -76,14 +76,14 @@
             </div>
         </div>
     </div>
-    <section class="container mx-auto mt-10">
+    <section class="container mx-auto mt-10">{{-- Publicaciones --}}
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
         @if ($posts->count()>0)
-        {{-- @if ($user->posts->count()>0)NO FUNCIONA PARA PAGINAR --}}
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl::grid-cols-4 gap-6">
                 @foreach ($posts as $post){{-- Lo usamos para paginar --}}
                     <div>
                         <a href="{{ route('posts.show',['post'=>$post,'user'=>$user]) }}">
+                            <p class="font-bold mb-3">{{ $post->titulo }}</p>{{-- Mostramos el título del post --}}
                             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
                         </a>
                         <p class="font-bold">{{-- Conteo de likes de la publicación --}}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,30 @@
 @endsection
 
 @section('contenido')
-    Contenido de la página principal
+
+    @if ($posts->count()){{-- Si existe algún post lo mostramos --}}
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl::grid-cols-4 gap-6">
+            @foreach ($posts as $post){{-- Lo usamos para paginar --}}
+                <div>
+                    <a href="{{ route('posts.show',['post'=>$post,'user'=>$post->user]) }}">
+                        <p class="font-bold mb-3">{{ $post->titulo }}</p>{{-- Mostramos el título del post --}}
+                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                        <p class="font-bold mt-4">Autor: {{ $post->user->username }}</p>{{-- Mostramos el autor del post --}}
+                    </a>
+                </div>
+            @endforeach
+        </div>
+        <div class="my-10">{{-- Lo usamos para paginar --}}
+            {{ $posts->links() }}
+        </div>
+    @else
+        <p class="text-center">No hay posts, sigue a alguien para poder mostrar sus posts</p>
+    @endif
+
+    {{-- @forelse ($posts as $post)
+        <h1>{{$post->titulo}}</h1>
+    @empty
+        <p>No hay posts</p>
+    @endforelse --}}
+
 @endsection
