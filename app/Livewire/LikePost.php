6117aed7febbc6bfbd98f8aa86b5462f9c4163f1
Carlos Muñoz <carlos.munoz.rodriguez@gmail.com>
2024-06-09 19:36:03 +0200

    Clase 168

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -9,6 +9,12 @@ class LikePost extends Component
     //public $mensaje = "Hola mundo desde un atributo"; /* La información la mandamos directamente desde aquí */
 
     public $post; /* La información le viene desde la vista */
+    public $isLiked; /* Guardará un booleano que almacenará si se le ha dado like o no a la publicación */
+    
+    public function mount($post) /* Es como el constructor de PHP */
+    {
+        $this->isLiked = $post->checkLike(auth()->user());
+    }
     
     public function like()
     {
@@ -18,6 +24,7 @@ class LikePost extends Component
                 ->likes()
                 ->where('post_id', $this->post->id)
                 ->delete();
+            $this->isLiked = false;
 
             return back();
         }
@@ -27,6 +34,7 @@ class LikePost extends Component
                 ->create([
                     'user_id' => auth()->user()->id
                 ]);
+            $this->isLiked = true;
         }
     }
     
